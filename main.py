8b27c48933bae66b4710feed54ab53bb12faf70a
Maks_Travel <maks.traveler@gmail.com>
2022-12-21 19:39:48 +0300

    Good

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 # This is a study Python script.
-
-def check_email(m):
-    '''This function for check valid email'''
-    sm = 0
-    dig = 0
-    alf = 0
-    simv = 0
-    for i in m:
-        if i.islower():
-           sm+=1
-        if i.isdigit():
-            dig +=1
-        if i.isupper():
-           alf +=1
-        if '@' in i and '.' in i:
-            simv +=1
-    if sm > 0 and dig >=0 and alf> 0 and simv > 0:
-         print('ДА')
-    else:
-        print('НЕТ')
+def chetn(x):
+    return x%2==0
 
 
-check_email('sc_lib@list.ru')
+while True:
+    x = int(input())
+    if x==1:
+        break
+    if chetn(x):
+        print(x)
